eb63f6fa02595fee458d43e9550c6cbb566c936b
Enes Baspinar <enesbaspinar07@gmail.com>
2021-08-02 17:14:38 +0300

    Add router and create Home page

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import HomePage from "./views/HomePage/HomePage";
+import "./App.scss";
 
 function App() {
   return (
-    <div></div>
+    <Router>
+      <Switch>
+        <Route path="/">
+          <HomePage />
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
